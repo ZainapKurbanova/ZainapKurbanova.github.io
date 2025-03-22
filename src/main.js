ec43a58e697887bb4aa380ab5d67e3e7f26f67c1
ZainapKurbanova <160218230+ZainapKurbanova@users.noreply.github.com>
2025-03-22 20:29:15 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
+import HeaderComponent from './view/header-component.js';
+import { render, RenderPosition } from './framework/render.js';
+
+
+const bodyContainer = document.querySelector('body');
+render(new HeaderComponent(), bodyContainer, RenderPosition.AFTERBEGIN);
 
