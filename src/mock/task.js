ebb765768e781df739ebc5777588ef798d660373
ZainapKurbanova <160218230+ZainapKurbanova@users.noreply.github.com>
2025-04-04 17:15:00 +0300

    Add files via upload

---
+++ src/mock/task.js
@@ -0,0 +1,62 @@
+export const tasks = [
+    {
+        id: "1",
+        title: "Сходить а магазин",
+        status: "backlog"
+    },
+    {
+        id: "2",
+        title: "Пойти погулять",
+        status: "backlog"
+    },
+    {
+        id: "3",
+        title: "Устроиться на работу",
+        status: "backlog"
+    },
+    {
+        id: "4",
+        title: "Устроиться на работу",
+        status: "in-progress",
+    },
+    {
+        id: "5",
+        title: "Выучить React",
+        status: "in-progress",
+    },
+    {
+        id: "6",
+        title: "Создать приложение на Vue.js",
+        status: "in-progress",
+    },
+    {
+        id: "7",
+        title: "Разработать сервер на .NET (C#)",
+        status: "done",
+    },
+    {
+        id: "8",
+        title: "Настроить маршрутизацию в React",
+        status: "done",
+    },
+    {
+        id: "9",
+        title: "Оптимизировать код на Vue.js",
+        status: "trash",
+    },
+    {
+        id: "10",
+        title: "Удалить ненужные файлы",
+        status: "trash",
+    },
+    {
+        id: "11",
+        title: "Очистить базу данных",
+        status: "trash",
+    },
+    {
+        id: "12",
+        title: "Изучить архитектуру .NET Core",
+        status: "trash",
+    },
+]
