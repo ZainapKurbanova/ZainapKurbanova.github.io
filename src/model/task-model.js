08f43c08da9332d2f32945f1a158703864012f4f
ZainapKurbanova <160218230+ZainapKurbanova@users.noreply.github.com>
2025-04-11 17:46:25 +0300

    Update task-model.js

--- src/model/task-model.js
+++ src/model/task-model.js
@@ -3,7 +3,11 @@ import {tasks} from '../mock/task.js';
 export default class TasksModel {
     #boardtasks = tasks;
 
-    getTasks() {
+    get tasks() {
         return this.#boardtasks;
+    }
+
+    getTasksByStatus(status) {
+        return this.#boardtasks.filter((task) => task.status === status);
     }
 }
