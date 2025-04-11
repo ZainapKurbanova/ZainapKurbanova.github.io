c580b3cc64c16b212698e2edb9fd2a3ecf5282e8
ZainapKurbanova <160218230+ZainapKurbanova@users.noreply.github.com>
2025-04-11 17:41:18 +0300

    Update render.js

--- src/framework/render.js
+++ src/framework/render.js
@@ -1,3 +1,5 @@
+import { AbstractComponent } from "../framework/view/abstract-component.js";
+
 const RenderPosition = {
     BEFOREBEGIN: 'beforebegin',
     AFTERBEGIN: 'afterbegin',
@@ -13,7 +15,13 @@ const RenderPosition = {
   }
   
   function render(component, container, place = RenderPosition.BEFOREEND) {
-  container.insertAdjacentElement(place, component.getElement());
+    if (!(component instanceof AbstractComponent)) {
+      throw new Error('Can render only components');
+    }
+    if (container === null) {
+      throw new Error('Container element doesn\'t exist');
+    }
+    container.insertAdjacentElement(place, component.element);
   }
   
   export {RenderPosition, createElement, render};
