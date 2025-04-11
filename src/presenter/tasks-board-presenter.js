9ad21f61f5fa106f32d976ee10d8cfaa2f08696a
ZainapKurbanova <160218230+ZainapKurbanova@users.noreply.github.com>
2025-04-11 17:47:07 +0300

    Update tasks-board-presenter.js

--- src/presenter/tasks-board-presenter.js
+++ src/presenter/tasks-board-presenter.js
@@ -1,9 +1,10 @@
 import TaskBoardComponent from '../view/taskboard-component.js';
 import TaskListComponent from '../view/task-list-component.js';
 import TaskComponent from '../view/task-component.js';
+import EmptyTaskListComponent from '../view/empty-task-list-component.js';
 import ClearBasketButtonComponent from '../view/clear-basket-button-component.js';
 import { render } from '../framework/render.js';
-import { Status, StatusLabel } from '../const.js';
+import { Status } from '../const.js';
 
 export default class TasksBoardPresenter {
   #boardContainer = null;
@@ -16,25 +17,58 @@ export default class TasksBoardPresenter {
   }
 
   init() {
+    this.#renderBoard();
+  }
+
+  #renderBoard() {
     render(this.#tasksBoardComponent, this.#boardContainer);
     
-    const tasks = this.#tasksModel.getTasks();
-    
-    Object.entries(Status).forEach(([statusKey, statusValue]) => {
-      const taskListComponent = new TaskListComponent(statusValue, StatusLabel[statusValue]);
-      render(taskListComponent, this.#tasksBoardComponent.getElement().querySelector('.tasks'));
-      
-      const taskListContainer = taskListComponent.getElement().querySelector('.task-list');
-  
-      tasks
-        .filter(task => task.status === statusValue)
-        .forEach(task => {
-          const taskComponent = new TaskComponent(task);
-          render(taskComponent, taskListContainer);
-        });
-      if (statusValue === Status.BASKET) {
-        render(new ClearBasketButtonComponent(), taskListContainer);
+    Object.values(Status).forEach((status) => {
+      this.#renderTasksList(status);
+      if (status === Status.BASKET) {
+        this.#renderClearBasketButton();
       }
     });
+  }
+
+  #renderTasksList(status) {
+    const taskListComponent = new TaskListComponent(status);
+    render(taskListComponent, this.#tasksBoardComponent.element.querySelector('.tasks'));
+    
+    const taskListContainer = taskListComponent.element.querySelector('.task-list');
+    const tasksForStatus = this.#getTasksByStatus(status);
+    
+    if (tasksForStatus.length === 0) {
+      this.#renderEmptyList(status, taskListContainer);
+    } else {
+      tasksForStatus.forEach((task) => {
+        this.#renderTask(task, taskListContainer);
+      });
+    }
+  }
+
+  #renderClearBasketButton() {
+    const status = Status.BASKET;
+    const tasksForStatus = this.#getTasksByStatus(status);
+    const basketListContainer = this.#tasksBoardComponent.element
+      .querySelector(`.${status} .task-list`);
+    
+    if (tasksForStatus.length > 0 && basketListContainer) {
+      render(new ClearBasketButtonComponent(), basketListContainer);
+    }
+  }
+
+  #renderTask(task, container) {
+    const taskComponent = new TaskComponent(task);
+    render(taskComponent, container);
+  }
+
+  #renderEmptyList(status, container) {
+    const emptyListComponent = new EmptyTaskListComponent(status);
+    render(emptyListComponent, container);
+  }
+
+  #getTasksByStatus(status) {
+    return this.#tasksModel.getTasksByStatus(status);
   }
 }
