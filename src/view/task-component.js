22236c386934e6d38f818e1eb6b16ee8e8b8767d
ZainapKurbanova <160218230+ZainapKurbanova@users.noreply.github.com>
2025-04-25 12:40:00 +0300

    feat(drag-drop): add data-task-id to task component template
    Add `data-task-id` attribute to the task item template in `task-component.js` to enable identification of tasks in the DOM. This supports precise task positioning during drag-and-drop operations.
    
    Required for determining the target task in `task-list-component.js` when inserting tasks at specific positions in the list.

--- src/view/task-component.js
+++ src/view/task-component.js
@@ -1,16 +1,34 @@
-import {  AbstractComponent } from '../framework/view/abstract-component.js';
+import { AbstractComponent } from '../framework/view/abstract-component.js';
 
 function createTaskTemplate(task) {
-  return `<li class="task-item">${task.title}</li>`;
+  return `<li class="task-item" data-task-id="${task.id}">${task.title}</li>`;
 }
 
 export default class TaskComponent extends AbstractComponent {
   constructor(task) {
     super();
     this._task = task;
+    this.#afterCreateElement();
   }
 
   get template() {
     return createTaskTemplate(this._task);
+  }
+
+  #afterCreateElement() {
+    this.#makeTaskDraggable();
+  }
+
+  #makeTaskDraggable() {
+    this.element.setAttribute('draggable', true);
+  
+    this.element.addEventListener('dragstart', (event) => {
+      event.dataTransfer.setData('text/plain', this._task.id);
+      this.element.classList.add('dragging');
+    });
+  
+    this.element.addEventListener('dragend', () => {
+      this.element.classList.remove('dragging');
+    });
   }
 }
